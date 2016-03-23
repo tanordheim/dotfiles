653fec73233fe28175174c95f46a6ec4fcf084a5
Trond Nordheim <trond@nordheim.io>
2016-03-23 16:00:21 +0100

    Install zprezto automatically

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 
 BASEDIR=$(pwd -P)
 
+if [ ! -d "$HOME/.zprezto" ]
+then
+  echo Installing zprezto
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
+  for i in $(ls $HOME/.zprezto/runcoms/*)
+  do
+    FILENAME=$(basename $i)
+
+    if [ "$FILENAME" != "README.md" ]
+    then
+      echo "Linking $i -> $HOME/.$FILENAME"
+      ln -sf "$i" "$HOME/.$FILENAME"
+    fi
+
+  done
+fi
+
 ln -sf $BASEDIR/gitignore_global ~/.gitignore_global
 ln -sf $BASEDIR/gitconfig ~/.gitconfig
 ln -sf $BASEDIR/zpreztorc ~/.zpreztorc
