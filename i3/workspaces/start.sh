a65def0479a7565a5d2f4ce95e7af55087dd52c1
Trond Nordheim <trond@nordheim.io>
2017-07-30 10:11:24 +0200

    Set up desktop 10 as well

--- i3/workspaces/start.sh
+++ i3/workspaces/start.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-WS=$1
-TERM=$2
+TERM=$1
+WS1=$2
+WS2=$3
+WS3=$4
+WS4=$5
+WS5=$6
+WS6=$7
+WS7=$8
+WS8=$9
+WS9="${10}"
+WS10="${11}"
 
 # Switch to workspace 2 and set up the layout
-i3-msg "workspace $WS"
+i3-msg "workspace $WS2"
 i3-msg "append_layout ~/.config/i3/workspaces/2.json"
 
-# Start workspace apps
+# Start workspace 2 apps
 $TERM &
 slack &
 firefox --new-instance \
@@ -16,3 +25,13 @@ firefox --new-instance \
         --new-tab --url https://mail.google.com/mail/u/1/ \
         --new-tab --url https://calendar.google.com/calendar/b/0/render \
         --new-tab --url https://calendar.google.com/calendar/b/1/render &
+
+# Switch to workspace 10 and set up the layout
+i3-msg "workspace $WS10"
+i3-msg "append_layout ~/.config/i3/workspaces/10.json"
+
+# Start workspace 10 apps
+enpass &
+
+# Default back to workspace 1
+i3-msg "workspace $WS1"
