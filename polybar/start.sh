0b1e500dbd3d496b5f9ca9fe768e37988dc26dd3
Trond Nordheim <trond@nordheim.io>
2018-02-21 09:47:24 +0100

    Adjust polybar depending on machine

--- polybar/start.sh
+++ polybar/start.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+HOST=$(hostname)
 killall polybar
-polybar primary &
-polybar secondary &
+
+export PRIMARY_MONITOR="DisplayPort-1"
+export SECONDARY_MONITOR="DisplayPort-2"
+
+if [ "$HOST" = "mirage" ]
+then
+	export PRIMARY_MONITOR="eDP-1"
+	export SECONDARY_MONITOR=""
+fi
+
+echo "primary=$PRIMARY_MONITOR"
+echo "secondary=$SECONDARY_MONITOR"
+
+if [ ! -z "$PRIMARY_MONITOR" ]
+then
+	echo "Launching primary polybar on monitor $PRIMARY_MONITOR"
+	polybar primary &
+fi
+
+if [ ! -z "$SECONDARY_MONITOR" ]
+then
+	echo "Launching secondary polybar on monitor $SECONDARY_MONITOR"
+	polybar secondary &
+fi
